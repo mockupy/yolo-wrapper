b1117082f6fad755dcab649d03e286499382f133
Behlul Bahadir Boluk <behlulboluk@gmail.com>
2019-04-01 19:59:40 +0300

    resized image

--- Script.py
+++ Script.py
@@ -1,16 +1,22 @@
 import argparse
 from subprocess import call
+import cv2
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", required=True, help="path to the input image")
 args = vars(parser.parse_args())
+
+img = cv2.imread(args["image"])
+img = cv2.resize(img,(508,704))
+img2 = cv2.imwrite("data/testbeh.jpg",img)
+
 darknet_path = 'darknet.exe detector test'
 datacfg = 'data/obj.data'
 cfgfile = 'yolo-obj.cfg'
 weightfile = 'yolo-obj_11000.weights'
-img = 'data/test4.jpg'
+img = 'data/testbeh.jpg'
 
-call(darknet_path + " " + datacfg + " " + cfgfile + " " + weightfile + " " + args["image"], shell=True)
+call(darknet_path + " " + datacfg + " " + cfgfile + " " + weightfile + " " + img, shell=True)
 
 f = open("BBoxOutputFile.txt","r")
 myList = []
